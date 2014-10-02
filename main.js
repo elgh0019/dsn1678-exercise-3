d6d0a845633053070d70af3130ef18f9f19d978b
elgh0019 <e.elghirani@hotmail.com>
2014-10-03 01:07:40 +1000

    making changes, editing java

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+var salesLeft = $('.sales-left');
+var detailsLeft = $('details-left');
+
+var salesLeftClickHandler = function () {
+	var currentState = detailsLeft.attr('data-state');
+	
+	if (currentState == 'active') {
+		detailsLeft.attr('data-state','inactive'); 
+	} else {
+		detailsLeft.attr('data-state', 'active');
+	}
+};
+
+salesLeft.on('click', salesLeftClickHandler);
